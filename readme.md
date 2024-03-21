12cb6c923e3b46c23d7f695a6a1e3bd7800b4e66
coolGi <sasanaps@hotmail.com>
2024-03-22 01:45:59 +1030

    Init commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Cosmic Quilt Example Mod
+> The example mod for the [Cosmic Quilt](https://codeberg.org/CRModders/cosmic-quilt) Loader
+
+## Notes
+- Most project properties can be changed in the `gradle.properties`
+- To change author, description and stuff that isnt there, edit `src/main/resources/quilt.mod.json`
+- The project name is defined in `settings.gradle`
---
+++ src/main/java/com/example/example_mod/mixins/MainMenuMixin.java
@@ -0,0 +1,17 @@
+package com.example.example_mod.mixins;
+
+import finalforeach.cosmicreach.gamestates.MainMenu;
+import org.quiltmc.loader.impl.util.log.Log;
+import org.quiltmc.loader.impl.util.log.LogCategory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MainMenu.class)
+public class MainMenuMixin {
+    @Inject(method = "create", at = @At("HEAD"))
+    private void injected(CallbackInfo ci) {
+        System.out.println("Example mixin logged!");
+    }
+}
